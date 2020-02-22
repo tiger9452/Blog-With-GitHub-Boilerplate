8f03ce2db4a65abc6e5efd6485904d6181b9fcd7
tiger9452 <61257940+tiger9452@users.noreply.github.com>
2020-02-22 19:06:39 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -19,13 +19,13 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "Sam的个人博客"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
+author = "tiger9452"
+email = "tiger9452@163.com"
 author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
+description = "工作即是生活，生活即是工作。"
 key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
